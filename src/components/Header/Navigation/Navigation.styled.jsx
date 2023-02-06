90c98067815da7e875e136aca13d980440dc9beb
Taras-Roboteckyi <rtvr@ukr.net>
2023-02-06 20:55:58 +0200

    fix navigation styled

--- src/components/Header/Navigation/Navigation.styled.jsx
+++ src/components/Header/Navigation/Navigation.styled.jsx
@@ -7,15 +7,24 @@ export const NavList = styled.ul`
 
 export const MenuItem = styled.li`
   transition: border-bottom 500ms;
-  padding-top: 5px;
-  padding-bottom: 5px;
+
+  position: relative;
 
   &:not(:last-child) {
     margin-right: 12px;
   }
   &:hover,
   &:focus {
-    border-bottom: 1px solid ${props => props.theme.colors.$styleColor};
+    &:after {
+      position: absolute;
+
+      content: '';
+      top: 100%;
+      left: 0;
+      height: 2px;
+      width: 100%;
+      border-bottom: 2px solid ${props => props.theme.colors.$styleColor};
+    }
   }
   @media (min-width: ${props => props.theme.breakpoints.tablet}) {
     &:not(:last-child) {
@@ -30,6 +39,9 @@ export const MenuItem = styled.li`
 `;
 
 export const MenuLink = styled.a`
+  padding-top: 5px;
+  padding-bottom: 5px;
+
   font-size: 16px;
   line-height: 22px;
   color: ${props => props.theme.colors.$white};
